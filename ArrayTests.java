e2e0457982aa461c0ef47e81e2ad3920171e04af
hebelou <156348244+hebelou@users.noreply.github.com>
2024-02-01 09:36:46 -0800

    Edit to files

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,50 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlaceSingle() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace1(){
+    int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3,2,1}, input1);
+  }
 
   @Test
-  public void testReversed() {
+  public void testReverseInPlace2(){
+    int[] input1 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2,1}, input1);
+  }
+
+  @Test
+  public void testReversedEmpty() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverse(){
+    int [] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+  public void testFilter(){
+    List<String> str = new ArrayList<>();
+    str.add("a"); str.add("b"); str.add("c");
+    List<String> strRev = new ArrayList<>();
+    strRev.add("c"); strRev.add("b"); strRev.add("a");
+    assertArrayEquals(strRev, ListExamples.filter(str, new StringChecker() sc));
+  }
+
+
 }
--- ListExamples.java
+++ ListExamples.java
@@ -3,11 +3,16 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
-class ListExamples {
+class ListExamples{
 
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
+  boolean checkString(String s){
+    if(s.length() % 2 == 0) return true;
+    else return false; 
+  }
+
   static List<String> filter(List<String> list, StringChecker sc) {
     List<String> result = new ArrayList<>();
     for(String s: list) {
